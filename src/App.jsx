415e6b2fc8bf40c4c92fa55660a55973744be08e
AnaRativa09 <anarativa09@gmail.com>
2021-08-02 18:34:27 -0500

    Create router

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+
+// Import views
+import Home from './views/Home';
+import Login from './views/Login';
+import SignUp from './views/SignUp';
+import Dashboard from './views/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          está es mi prueba.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/dashboard" component={Dashboard} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
