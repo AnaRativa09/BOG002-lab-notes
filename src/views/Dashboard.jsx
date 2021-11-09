f8f72c31d5a9f77e5e18ddd78eb5a98d5456bc09
AnaRativa09 <anarativa09@gmail.com>
2021-11-09 10:58:42 -0500

    feat: add signOut function

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,11 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import { getNotes } from '../controller/firestore';
+import { useHistory } from 'react-router-dom';
+import { getNotes } from '../controller/firebaseFirestore';
+import { signOutUser } from '../controller/firebaseAuth';
+
 import FormNotes from '../components/formNotes';
 
 import '../styles/Dashboard.css';
 
 export default function Dashboard() {
+  const history = useHistory();
+
   const [dataNotes, setDataNotes] = useState([]);
+
+  const userLogOut = () => {
+    signOutUser()
+      .then(() => {
+        console.log('Cerrando sesión')
+        history.push('/');
+      })
+      .catch(err => console.error(err));
+  }
 
   useEffect(() => {
     getNotes('notes', setDataNotes);
@@ -13,6 +27,7 @@ export default function Dashboard() {
 
   return (
     <>
+      <button type="button" onClick={userLogOut}>Cerrar sesión</button>
       <h2>Aquí ves y creas notas</h2>
 
       <FormNotes />
