8101d32780aa974b6209afea45cecaeea027f871
AnaRativa09 <anarativa09@gmail.com>
2021-11-23 19:14:19 -0500

    feat: add authprovider to router, create privateRoute

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import './App.css';
+import { AuthProvider } from './controller/authContext';
 
+import PrivateRoute from './components/PrivateRoute';
 // Import views
-import Home from './views/Home';
+import Welcome from './views/Welcome';
 import Login from './views/Login';
 import SignUp from './views/SignUp';
 import Dashboard from './views/Dashboard';
 
+import './App.css';
+
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={SignUp} />
-        <Route path="/dashboard" component={Dashboard} />
-      </Switch>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Welcome} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={SignUp} />
+          <PrivateRoute path="/dashboard" component={Dashboard} />
+        </Switch>
+      </BrowserRouter>
+    </AuthProvider>
+
   );
 }
 
